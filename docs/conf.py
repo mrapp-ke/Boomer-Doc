b152fdba2673b754debc70732964a4ed256d8860
Michael Rapp <michael.rapp90@googlemail.com>
2021-09-01 16:20:07 +0200

    Update Sphinx configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../python/mlrl'))
 
 
 # -- Project information -----------------------------------------------------
@@ -30,7 +27,7 @@ release = '0.6.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
